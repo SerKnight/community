3893af8e98a77420c8f84ef1de4541f154bd692e
Jon Maser <jon@advlo.com>
2016-03-28 17:23:27 -0500

    bug fixes and seo tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+	def default_meta_tags
+		{
+		  :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
+		  :description    => 'Discover unique group adventures created by travelers with local guides.',
+		}
+	end
+
+	def default_og_tags
+	    return {
+	      :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
+	      :type           => 'website',
+	      :url            => "#{root_url}",
+	      :description    => 'Discover unique group adventures created by travelers with local guides.',
+	      :image          => 'http://imgur.com/cE77vwg'
+	    }
+	end
+
+	def default_twitter_tags
+	    return {
+	      :card           => 'summary',
+	      :site           => '@adventuremade',
+	      :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
+	      :description    => 'Discover unique group adventures created by travelers with local guides.',
+	      :image          => {
+	                          :_ => 'http://imgur.com/cE77vwg'
+	                        }
+	    }
+	end
 end
