a9ddc90199dad6f94dd468810e1576e0eae30448
Jon Maser <jon@advlo.com>
2016-03-29 13:00:10 -0500

    fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,3 @@
 module ApplicationHelper
 
-	def default_meta_tags
-	    {
-	      :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
-	      :description    => 'Discover unique group adventures created by travelers with local guides.'
-	    }
-  	end
-
-  def default_og_tags
-    return {
-      :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
-      :type           => 'website',
-      :url            => "#{root_url}",
-      :description    => 'Discover unique group adventures created by travelers with local guides.',
-      :image          => "http://i.imgur.com/u3vGbYZ.jpg"
-    }
-  end
-
-  def default_twitter_tags
-    return {
-      :card           => 'summary',
-      :site           => '@AdventureMade',
-      :title          => 'AdventureMade - One of a Kind Collaborative Group Adventures',
-      :description    => 'Discover unique group adventures created by travelers with local guides.',
-      :image          => {
-                          :_ => "http://i.imgur.com/u3vGbYZ.jpg"
-                        }
-    }
-  end
 end
